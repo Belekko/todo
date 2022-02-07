1ca07f13ba5265f438356fec7ee981a2a5dd50c3
Belekko <m.u.belekpr@gmail.com>
2022-02-07 11:22:24 +0600

    'ok'

--- src/App.js
+++ src/App.js
@@ -3,40 +3,40 @@ import "./App.css";
 import AddTodo from "./components/AddTodo";
 import Todos from "./components/Todos";
 
+const reducer = (state, action) => {
+    switch (action.type) {
+        case "add":
+            return [
+                ...state,
+                {
+                    id: Date.now(),
+                    name: action.payload.name,
+                    completed: false,
+                },
+            ];
+
+        case "checked":
+            return state.map((todo) => {
+                if (todo.id === action.payload.id) {
+                    return { ...todo, completed: !todo.completed };
+                }
+                return todo;
+            });
+        case "delete":
+            return state.filter((todo) => todo.id !== action.payload.id);
+        case "edit":
+            return state.map((el) => {
+                if (el.id === action.payload.id) {
+                    el.name = action.payload.editName;
+                }
+                return el;
+            });
+        default:
+            return state;
+    }
+};
+
 function App() {
-    const reducer = (state, action) => {
-        switch (action.type) {
-            case "add":
-                return [
-                    ...state,
-                    {
-                        id: Date.now(),
-                        name: action.payload.name,
-                        completed: false,
-                    },
-                ];
-
-            case "checked":
-                return state.map((todo) => {
-                    if (todo.id === action.payload.id) {
-                        return { ...todo, completed: !todo.completed };
-                    }
-                    return todo;
-                });
-            case "delete":
-                return state.filter((todo) => todo.id !== action.payload.id);
-            case "edit":
-                return state.map((el) => {
-                    if (el.id === action.payload.id) {
-                        el.name = action.payload.editName;
-                    }
-                    return el;
-                });
-            default:
-                return state;
-        }
-    };
-
     const localData = JSON.parse(localStorage.getItem("task"));
     const [todos, dispatch] = useReducer(reducer, localData ? localData : []);
 
